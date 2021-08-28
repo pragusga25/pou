38aee9838bbce8587a8f189374725549f677b2d3
Taufik Pragusga <pragusga25@gmail.com>
2021-08-29 06:06:58 +0700

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import Layout from '@components/Layout'
 import Loader from '@components/Loader'
 import HomePage from '@pages/Home'
-import React, { Suspense } from 'react'
+import React, { Suspense, useState, useEffect } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
+
+  if (loading) return <Loader />
+
   return (
     <Suspense fallback={<Loader />}>
       <BrowserRouter>
--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -5,8 +5,8 @@ import { defaultOptions } from '@constants/defaultOptions'
 
 const Loader: React.FC = () => {
   return (
-    <div className="min-h-screen w-full flex justify-center items-center">
-      <Lottie options={{ ...defaultOptions, animationData: loader }} width={500} height={500} />
+    <div className="bg-[#161d20] min-h-screen w-full flex justify-center items-center">
+      <Lottie options={{ ...defaultOptions, animationData: loader, loop: true }} width={500} height={500} />
     </div>
   )
 }
